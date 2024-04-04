9cedd5f7a6d193b4c684671f99228ff00d2eea44
Yohannes Laksana <yoholaksana@gmail.com>
2024-04-04 13:29:43 -0500

    made gradle changes and added action bar

--- app/src/main/java/com/example/apfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/apfinalproject/MainActivity.kt
@@ -5,112 +5,106 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.apfinalproject.databinding.ActionBarBinding
+import com.example.apfinalproject.databinding.ActivityMainBinding
+import com.example.apfinalproject.MainActivity
+import com.example.apfinalproject.ui.HomeFragment
+import com.example.apfinalproject.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-//    private var actionBarBinding: ActionBarBinding? = null
-//    private val viewModel: MainViewModel by viewModels()
-//    private lateinit var navController : NavController
-//
-//    // An Android nightmare
-//    // https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
-//    // https://stackoverflow.com/questions/7789514/how-to-get-activitys-windowtoken-without-view
-//    fun hideKeyboard() {
-//        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0)
-//    }
-//    }
-//
-//    // https://nezspencer.medium.com/navigation-components-a-fix-for-navigation-action-cannot-be-found-in-the-current-destination-95b63e16152e
-//    // You get a NavDirections object from a Directions object like
-//    // HomeFragmentDirections.
-//    // safeNavigate checks if you are in the source fragment for the directions
-//    // object and if not does nothing
-//    private fun NavController.safeNavigate(direction: NavDirections) {
-//        currentDestination?.
-//        getAction(direction.actionId)?.
-//        run {
-//            navigate(direction)
-//        }
-//    }
-//    private fun actionBarTitleLaunchSubreddit() {
-//        // XXX Write me actionBarBinding, safeNavigate
-//        actionBarBinding?.actionTitle?.setOnClickListener {
-//            navController.safeNavigate(HomeFragmentDirections.actionHomeFragmentToSubreddits())
-//        }
-//    }
-//    private fun actionBarLaunchFavorites() {
-//        // XXX Write me actionBarBinding, safeNavigate
-//        actionBarBinding?.actionFavorite?.setOnClickListener {
-//            navController.safeNavigate(HomeFragmentDirections.actionHomeFragmentToFavorites())
-//        }
-//    }
-//
-//    // XXX check out addTextChangedListener
-//    private fun actionBarSearch() {
-//        // XXX Write me
-//        actionBarBinding?.actionSearch?.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                val text = s.toString()
-//                // Do something with the text
-//                Log.d("netSubreddits",  "${viewModel.subreddits}")
-//                Log.d("netPosts",  "${viewModel.posts}")
-//                viewModel.setSearchTerm(text)
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//            }
-//        })
-//
-//    }
-//
-//    private fun initDebug() {
-//        if(globalDebug) {
-//            assets.list("")?.forEach {
-//                Log.d(javaClass.simpleName, "Asset file: $it" )
-//            }
-//            jsonAww100 = assets.open("aww.hot.1.100.json.transformed.txt").bufferedReader().use {
-//                it.readText()
-//            }
-//            subreddit1 = assets.open("subreddits.1.json.txt").bufferedReader().use {
-//                it.readText()
-//            }
-//        }
-//    }
-//    private fun initTitleObservers() {
-//        // Observe title changes
-//        viewModel.observeTitle().observe(this) { newTitle ->
-//            Log.d("Changing title", "$newTitle")
-//            actionBarBinding?.actionTitle?.text = newTitle
-//        }
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(activityMainBinding.root)
-//        setSupportActionBar(activityMainBinding.toolbar)
-//        supportActionBar?.let{
-//            initActionBar(it)
-//        }
-//
-//        initDebug()
-//        initTitleObservers()
-//        actionBarTitleLaunchSubreddit()
-//        actionBarLaunchFavorites()
-//        actionBarSearch()
+    private var actionBarBinding: ActionBarBinding? = null
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var navController : NavController
+
+    // An Android nightmare
+    // https://stackoverflow.com/questions/1109022/close-hide-the-android-soft-keyboard
+    // https://stackoverflow.com/questions/7789514/how-to-get-activitys-windowtoken-without-view
+    fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0)
+    }
+
+
+    // https://nezspencer.medium.com/navigation-components-a-fix-for-navigation-action-cannot-be-found-in-the-current-destination-95b63e16152e
+    // You get a NavDirections object from a Directions object like
+    // HomeFragmentDirections.
+    // safeNavigate checks if you are in the source fragment for the directions
+    // object and if not does nothing
+    private fun NavController.safeNavigate(direction: NavDirections) {
+        currentDestination?.
+        getAction(direction.actionId)?.
+        run {
+            navigate(direction)
+        }
+    }
+    private fun actionBarTitleLaunchSubreddit() {
+        // XXX Write me actionBarBinding, safeNavigate
+        actionBarBinding?.actionTitle?.setOnClickListener {
+            navController.safeNavigate(HomeFragmentDirections.actionHomeFragmentToSubreddits())
+        }
+    }
+    private fun actionBarLaunchFavorites() {
+        // XXX Write me actionBarBinding, safeNavigate
+        actionBarBinding?.actionFavorite?.setOnClickListener {
+            navController.safeNavigate(HomeFragmentDirections.actionHomeFragmentToFavorites())
+        }
+    }
+
+    // XXX check out addTextChangedListener
+    private fun actionBarSearch() {
+        // XXX Write me
+        actionBarBinding?.actionSearch?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val text = s.toString()
+                // Do something with the text
+                Log.d("netSubreddits",  "${viewModel.subreddits}")
+                Log.d("netPosts",  "${viewModel.posts}")
+                viewModel.setSearchTerm(text)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+    }
+    private fun initTitleObservers() {
+        // Observe title changes
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.toolbar)
+        supportActionBar?.let{
+            initActionBar(it)
+        }
+
+        initTitleObservers()
+        actionBarTitleLaunchSubreddit()
+        actionBarLaunchFavorites()
+        actionBarSearch()
 //
 //
-//        // Set up our nav graph
-//        navController = findNavController(R.id.main_frame)
-//        val appBarConfiguration = AppBarConfiguration(navController.graph)
-//        // If we have a toolbar (not actionbar) we don't need to override
-//        // onSupportNavigateUp().
-//        activityMainBinding.toolbar.setupWithNavController(navController, appBarConfiguration)
-//        //setupActionBarWithNavController(navController, appBarConfiguration)
-//    }
+        // Set up our nav graph
+        navController = findNavController(R.id.main_frame)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        // If we have a toolbar (not actionbar) we don't need to override
+        // onSupportNavigateUp().
+        activityMainBinding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
+    }
 }
--- app/src/main/java/com/example/apfinalproject/ui/MainViewModel.kt
+++ app/src/main/java/com/example/apfinalproject/ui/MainViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.apfinalproject.ui
 
-class MainViewModel {
+import android.util.Log
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+class MainViewModel: ViewModel() {
 
 }

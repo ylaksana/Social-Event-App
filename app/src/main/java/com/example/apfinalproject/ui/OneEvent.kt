5171cfbf926d8f0366190ae72e2e1b740cca9fd3
Yohannes Laksana <yoholaksana@gmail.com>
2024-04-04 19:50:18 -0500

    oneEvent

--- app/src/main/java/com/example/apfinalproject/ui/OneEvent.kt
+++ app/src/main/java/com/example/apfinalproject/ui/OneEvent.kt
@@ -1,7 +1,9 @@
 package com.example.apfinalproject.ui
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 
 class OneEvent: Fragment(){
+    private val viewModel : MainViewModel by activityViewModels()
 
 }

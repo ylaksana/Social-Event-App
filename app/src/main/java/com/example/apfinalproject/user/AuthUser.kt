61092d82cb7ef6e14334f01096fb94b713b1e422
Eric Ravet <67251881+eravet@users.noreply.github.com>
2024-04-06 12:54:31 -0500

    add firebase auth

---
+++ app/src/main/java/com/example/apfinalproject/user/AuthUser.kt
@@ -0,0 +1,86 @@
+package com.example.apfinalproject.user
+
+import android.content.Intent
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class AuthUser(private val registry: ActivityResultRegistry) :
+    DefaultLifecycleObserver,
+    FirebaseAuth.AuthStateListener {
+    companion object {
+        private const val TAG = "AuthUser"
+    }
+    private lateinit var signInLauncher: ActivityResultLauncher<Intent>
+    private var activeUser = MutableLiveData<User>().apply {
+        this.postValue(invalidUser)
+    }
+    private var pendingLogin = false
+    init {
+        // Listen to FirebaseAuth state
+        // That way, if the server logs us out, we know it and change the view
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+    }
+
+    fun observeActiveUser(): MutableLiveData<User> {
+        return activeUser
+    }
+
+    // Update active user LiveData upon a change of state for our FirebaseUser
+    private fun activeUserUpdate(firebaseUser: FirebaseUser?) {
+        if(firebaseUser == null) {
+            activeUser.postValue(invalidUser)
+            login()
+        } else {
+            val user = User(firebaseUser.displayName,
+                firebaseUser.email, firebaseUser.uid)
+            activeUser.postValue(user)
+        }
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+        signInLauncher = registry.register("key", owner,
+            FirebaseAuthUIActivityResultContract()) { result ->
+                Log.d(TAG, "sign in result ${result.resultCode}")
+                pendingLogin = false
+            }
+    }
+
+    override fun onAuthStateChanged(p0: FirebaseAuth) {
+        Log.d(TAG, "onAuthStateChanged null? ${p0.currentUser == null}")
+        activeUserUpdate(p0.currentUser)
+    }
+
+    private fun user(): FirebaseUser? {
+        return Firebase.auth.currentUser
+    }
+
+    private fun login() {
+        if (user() == null && !pendingLogin) {
+            pendingLogin = true
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+            )
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .build()
+            signInLauncher.launch(signInIntent)
+        }
+    }
+
+    fun logout() {
+        if(user() == null) return
+        Firebase.auth.signOut()
+    }
+}
--- app/src/main/java/com/example/apfinalproject/user/User.kt
+++ app/src/main/java/com/example/apfinalproject/user/User.kt
@@ -4,11 +4,20 @@ import androidx.lifecycle.MutableLiveData
 import com.example.apfinalproject.event.Event
 import com.example.apfinalproject.interest.InterestCategories.Interest
 
-class User(var name: String) {
+class User(private val nullableName: String?,
+           private val nullableEmail: String?,
+           val uid: String) {
+    val name: String = nullableName ?: "User logged out"
+    val email: String = nullableEmail ?: "User logged out"
+
     var bio: String = "Wow... nothing's here yet!"
     var profilePicture: String = ""
     var userInterests: MutableLiveData<MutableList<Interest>> = MutableLiveData(mutableListOf())
     var pastEvents: MutableLiveData<MutableList<Event>> = MutableLiveData(mutableListOf())
+
+    fun isInvalid(): Boolean {
+        return this.uid == invalidUserUid
+    }
 
     fun addInterest(interest: Interest) {
         if (!userInterests.value?.contains(interest)!!) {
@@ -27,3 +36,5 @@ class User(var name: String) {
         pastEvents.value?.remove(event)
     }
 }
+const val invalidUserUid = "-1"
+val invalidUser = User(null, null, invalidUserUid)

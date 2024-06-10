da2a2e63b1e6cdd9449ce98d36f50b335227fdc1
Yohannes Laksana <yoholaksana@gmail.com>
2024-06-10 00:15:03 -0500

    created fragments for login

---
+++ app/src/main/java/com/example/apfinalproject/fragments/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.apfinalproject.fragments
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+class LoginFragment: Fragment() {
+}
